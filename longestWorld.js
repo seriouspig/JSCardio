74e87754786509ca2b4e4a87b4b9925c3d11ec22
seriouspig <pigryko@yahoo.com>
2021-07-11 11:17:57 +0200

    Longest Word 2

--- longestWorld.js
+++ longestWorld.js
@@ -28,6 +28,28 @@ function longestWord(sen) {
         return wordArray
     } else return wordArray[0]
 
-    
 }
 console.log(longestWord("Jas poszedl do kina szybko"))
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function longestWord2(sen) {
+    // create filtered array - remove comas and punctuation
+    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g)
+    // sort by length
+    const sorted = wordArr.sort(function(a,b) {
+        return b.length - a.length
+    })
+    // if multiple words, put into array
+    const longestWordArr = sorted.filter(function(word) {
+        return word.length === sorted[0].length
+    })
+    // check if more than one array val
+    if (longestWordArr.length === 1) {
+        return longestWordArr[0]
+    } else {
+        return longestWordArr
+    }
+}
+
+console.log(longestWord2("Hello, my name is: Jan"))
