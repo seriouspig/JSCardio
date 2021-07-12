20d75b52d69970fc437be4412f26edbcea4eca8c
seriouspig <pigryko@yahoo.com>
2021-07-12 16:21:04 +0200

    All flatten Arrays

--- flattenArray.js
+++ flattenArray.js
@@ -11,3 +11,29 @@ function flattenArray(arrays) {
 
 console.log(flattenArray([[0,1],[2,3,4],[5,6]]))
 console.log([0,1,2,3,4,5,6])
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function flattenArray2(arrays) {
+    return arrays.reduce(function(a, b) {
+        return a.concat(b)
+    })
+}
+
+console.log(flattenArray2([[0,1],[2,3,4],[5,6]]))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function flattenArray3(arrays) {
+    return [].concat.apply([], arrays)
+}
+
+console.log(flattenArray3([[0,1],[2,3,4],[5,6]]))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function flattenArray4(arrays) {
+    return [].concat(...arrays)
+}
+
+console.log(flattenArray4([["a","b"],["c","d","e"],["f","g"]]))
